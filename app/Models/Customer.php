9ab022adc20877fa0c7ac658839770413f39d481
Eldor <eldor@example.com>
2020-10-16 22:49:28 +0500

    BuyEvent notification class (continued); Notifiable Customer;

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class Customer extends Model
 {
+    use Notifiable;
+    
     protected $table = 'customers';
 
     /**
@@ -16,4 +20,27 @@ class Customer extends Model
     protected $fillable = [
         'phone', 'email',
     ];
+
+    /**
+     * Get the notification routing information for the given driver.
+     *
+     * @param  string  $driver
+     * @param  \Illuminate\Notifications\Notification|null  $notification
+     * @return mixed
+     */
+    public function routeNotificationFor($driver, $notification = null)
+    {
+        if (method_exists($this, $method = 'routeNotificationFor'.Str::studly($driver))) {
+            return $this->{$method}($notification);
+        }
+
+        switch ($driver) {
+            case 'database':
+                return $this->notifications();
+            case 'mail':
+                return $this->email;
+            case 'nexmo':
+                return $this->phone;
+        }
+    }
 }
--- app/Notifications/BuyEvent.php
+++ app/Notifications/BuyEvent.php
@@ -6,19 +6,24 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use App\Models\Purchase;
 
 class BuyEvent extends Notification
 {
     use Queueable;
+
+    private $purchase;
+    private $purchaseUrl;
 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Purchase $purchase)
     {
-        //
+        $this->purchase = $purchase;
+        $this->purchaseUrl = url('/purchases/' . $this->purchase->id);
     }
 
     /**
@@ -29,7 +34,7 @@ class BuyEvent extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['database'/*, 'mail', 'nexmo'*/];
     }
 
     /**
@@ -41,13 +46,19 @@ class BuyEvent extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('Your purchase was successful.')
-                    ->action('Go to the website', url('/'))
-                    ->line('Thank you!');
+            ->subject('Your purchase was successful.')
+            ->line('')
+            ->line('Details:')
+            ->line('Product title: ' . $this->purchase->product_title)
+            ->line('Purchase price: ' . $this->purchase->purchase_price)
+            ->line('')
+            ->action('Go to the website', $this->purchaseUrl)
+            ->line('')
+            ->line('Thank you!');
     }
 
     /**
-     * Get the array representation of the notification.
+     * Get the array representation of the notification (database and broadcasting).
      *
      * @param  mixed  $notifiable
      * @return array
@@ -55,7 +66,22 @@ class BuyEvent extends Notification
     public function toArray($notifiable)
     {
         return [
-            //
+            'product_title' => $this->purchase->product_title,
+            'purchase_price' => $this->purchase->purchase_price,
+            'purchase_url' => $this->purchaseUrl,
         ];
+    }
+
+    /**
+     * Get the Nexmo / SMS representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return NexmoMessage
+     */
+    public function toNexmo($notifiable)
+    {
+        return (new NexmoMessage)
+            ->content('Your purchase was successful. Product: ' . $this->purchase->product_title . ', Price: ' . $this->purchase->purchase_price . '. Thank you! (' . $this->purchaseUrl . ')')
+            ->unicode();
     }
 }
